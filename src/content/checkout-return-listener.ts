3d6307907c31d6dde50c1b724d07bae4797a7674
jamesrees77 <james@gobunk.co.uk>
2026-02-03 16:16:46 +0000

    fix content script

--- src/content/checkout-return-listener.ts
+++ src/content/checkout-return-listener.ts
@@ -2,8 +2,10 @@
     const url = new URL(location.href);
   
     const isSuccess =
+    url.searchParams.get("status") === "success" ||
       url.searchParams.has("be_success") ||
       url.pathname.startsWith("/success");
+
   
     if (!isSuccess) return;
   
